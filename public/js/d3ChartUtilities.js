28e1375a5fcb17573ad243ab268edb6b686424ef
Nigel Wade <nigel@dialexy.com>
2016-12-19 16:20:18 +0000

    Add extra endpoints, update column  chart

--- public/js/d3ChartUtilities.js
+++ public/js/d3ChartUtilities.js
@@ -7,7 +7,7 @@ const d3ChartUtilities = {
      * @param container is a dom element that the SVG element will be created in
      * @param props is an object for various miscellaneous
      */
-    drawColumnChart: function (data, labels, target, container, props) {
+    drawColumnChart: function (data, seriesLabels, tickLabels, target, container, props) {
         var colors = [
             '#1f78b4', '#33a02c', '#e31a1c', '#ff7f00', '#6a3d9a', '#b15928',
             '#62a3d0', '#72bf5b', '#ef5a5a', '#fe9f37', '#9a77b8', '#d8ac60',
@@ -26,25 +26,31 @@ const d3ChartUtilities = {
             yAxisWidth = 0,
             gap = 0;
 
-        //For multi series data
+        //For multi series data:
+        //Collate the values from multiple series into one array to find a global maximum
+        //Collate the lengths from multiple series to find the longest series
         var _dataCollate = {max: [], length: []};
         data.forEach(function (element, index, array) {
             _dataCollate.max = _dataCollate.max.concat(element);
             _dataCollate.length.push(element.length)
         });
 
+        //If the legend is specified then draw it
         if (props.displayLegend) {
-            legend.drawLegend(data, labels, colors, container, layout);
+            legend.drawLegend(data, seriesLabels, colors, container, layout);
             legendWidth = $('.legend', container).width();
         }
-        var containerSelection = d3.select(container);
 
+        //Create a d3 selection for the constainer of the chart
+        var containerSelection = d3.select(container);
+        //Create an SVG element for the chart
         var chart = containerSelection.append("svg")
             .attr("width", width - legendWidth)
             .attr("height", height)
             .html("")
             .attr("class", target);
 
+        //If there is a chart title specified draw it    
         if (props.chartTitle && props.chartTitle.length > 0) {
             var chartTitle = chart.append("text")
                 .classed("chart-title", true)
@@ -56,6 +62,7 @@ const d3ChartUtilities = {
                 });
             chartTitleHeight = chartTitle[0][0].offsetHeight;
         }
+        //If there is a caption specified draw it
         if (props.captionText && props.captionText.length > 0) {
             var captionText = chart.append("text")
                 .classed("caption-text", true)
@@ -67,19 +74,16 @@ const d3ChartUtilities = {
                 });
             captionTextHeight = captionText[0][0].offsetHeight;
         }
-
+        //First pass at creating x axis
         var x = d3.scaleBand()
-            .range([0, width - legendWidth - leftPadding - rightPadding])
-            .domain(labels[0]);
-        //var x_axis = d3.svg.axis().scale(x);
+            .rangeRound([0, width - legendWidth - leftPadding - rightPadding])
+            .domain(tickLabels[0]);
         var axis_x = containerSelection.select("svg")
             .append("g")
             .attr("class", "x axis")
             .call(d3.axisBottom(x))
             .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
+            .style("text-anchor", "center")
             .attr("transform", function (d) {
                 return "rotate(-" + props.xAxisTickLabelRotation + ")"
             });
@@ -91,7 +95,6 @@ const d3ChartUtilities = {
         var y = d3.scaleLinear()
             .range([plotHeight, 0]);
         y.domain([0, d3.max(_dataCollate.max)]);
-        //var y_axis = d3.svg.axis().scale(y).orient("left").tickFormat(d3.format("s"));
         var axis_y = containerSelection.select("svg")
             .append("g")
             .attr("class", "y axis")
@@ -99,10 +102,10 @@ const d3ChartUtilities = {
         yAxisWidth = axis_y['_groups'][0][0].getBBox().width;
         containerSelection.select('g.x.axis').remove();
 
+        //Second pass at x axis now we know how wide the y axis is.
         x = d3.scaleBand()
-            .range([0, width - legendWidth - yAxisWidth - leftPadding - rightPadding], 0, 0)
-            .domain(labels[0]);
-        //x_axis = d3.svg.axis().scale(x);
+            .rangeRound([0, width - legendWidth - yAxisWidth - leftPadding - rightPadding], 0, 0)
+            .domain(tickLabels[0]);
         axis_x = containerSelection.select("svg")
             .append("g")
             .attr("class", "x axis")
@@ -118,33 +121,34 @@ const d3ChartUtilities = {
 
         axis_y.attr("transform", "translate(" + yAxisWidth + leftPadding + ", " + (chartTitleHeight + captionTextHeight + topPadding) + ")");
 
+        //Work out various values
         var plotWidth = width - yAxisWidth - leftPadding - rightPadding;
 
         var barWidth = x.bandwidth() / data.length;
         var seriesWidth = x.bandwidth();
         var gap = props.multiSeriesGap * 0.01 * barWidth;
 
+        //Iterate over series and draw them
         for (var i = 0; i < data.length; i++) {
 
             var bar = [];
+            //Create groups for each data point
             bar[i] = chart.selectAll(".series")
                 .data(data[i])
                 .enter()
                 .append("g")
                 .attr("transform", function (d, idx) {
-                    return "translate(" + ((yAxisWidth + leftPadding) + (x(labels[i][idx]) + ((barWidth - gap) * i)) + gap) + ",0)";
+                    return "translate(" + ((yAxisWidth + leftPadding) + (x(tickLabels[i][idx]) + ((barWidth - gap) * i)) + gap) + ",0)";
                 });
 
+            //Add rect to group to represent data point
             bar[i].append("rect")
                 .classed("column", true)
                 .attr("data-name", function (d, idx) {
-                    return labels[i][idx];
+                    return seriesLabels[i];
                 })
                 .attr("data-value", function (d) {
                     return d;
-                })
-                .attr("data-series", function (d) {
-                    return 'Series-' + i;
                 })
                 .attr("y", function (d) {
                     return y(d) + chartTitleHeight + captionTextHeight + topPadding;
@@ -159,7 +163,7 @@ const d3ChartUtilities = {
 
             bar[i].append("title")
                 .text(function (d, idx) {
-                    return [d.name, ": ", d.value, " (", labels[i], ")"].join('');
+                    return [d, ' ', tickLabels[i][idx]].join('');
                 });
 
         }
